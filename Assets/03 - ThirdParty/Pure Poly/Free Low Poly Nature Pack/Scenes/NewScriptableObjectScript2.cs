286c350a9393f6b89de7c8d2b1b8c693866bbf39
Tom Bihoreau <82575721+tombihoreau@users.noreply.github.com>
2026-02-24 16:57:07 +0100

    player controller

---
+++ Assets/03 - ThirdParty/Pure Poly/Free Low Poly Nature Pack/Scenes/NewScriptableObjectScript2.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewScriptableObjectScript2", menuName = "Scriptable Objects/NewScriptableObjectScript2")]
+public class NewScriptableObjectScript2 : ScriptableObject
+{
+    
+}
